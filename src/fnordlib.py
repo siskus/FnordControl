3735b92b3b94c2e0a8823361f3eac5c2aa80ae12
Markus Mueller <siskus@gmail.com>
2010-09-18 14:30:17 +0200

    Copyright information added

--- src/fnordlib.py
+++ src/fnordlib.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8-*-
 
+#         fnordlib
+#
+# (c) by Markus Müller <siskus@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from serial import Serial, EIGHTBITS, STOPBITS_ONE
 from threading import Lock
 
--- src/server.py
+++ src/server.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8-*-
 
+#         fnordlight server
+#
+# (c) by Markus Müller <siskus@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 from fnordlib import FnordBus
 
 SERVER_PORT = 8000
-IP_ADRESS = '192.168.178.35'
+IP_ADRESS = '127.0.0.1'
 BUS_PORT = "/dev/ttyUSB0"
 STEP = 20
 DEBUG = 1
