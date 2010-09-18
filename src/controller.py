8addbd070716a1c68abbf28f1416b944a2f7fe06
Markus Mueller <siskus@gmail.com>
2010-09-18 14:22:27 +0200

    Bugfixing

--- src/controller.py
+++ src/controller.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8-*-
 
 from fnordlib import FnordBus
@@ -15,9 +15,18 @@ bus = FnordBus(PORT)
 bus.sync()
 bus.stop()
 
-for x in range(1000):
-    
-    value = (x * 20) % 256
-    bus.fade_rgb(255, value, value, value)
-    
-    sleep(1)
+#bus.fade_rgb2(255, 255, 255, 255)
+
+print("Program running...")
+
+try:
+    for x in range(1000):
+        
+        value = (x * 20) % 256
+        bus.fade_rgb(255, value, value, value)
+        
+        sleep(1)
+        
+except KeyboardInterrupt:
+    print("Shutting down...\n")
+    bus.black()
--- src/fnordlib.py
+++ src/fnordlib.py
@@ -1,76 +1,180 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8-*-
 
-from serial import Serial
+from serial import Serial, EIGHTBITS, STOPBITS_ONE
+from threading import Lock
+
+LIGHTCOUNT = 8
+DEBUG = 1
 
 class FnordBus:
     
     con = None
     
+    lock = None
+    
     red = 0
     green = 0
     blue = 0
+    
+    lights = None
     
     def __init__(self, serial_port):
         
         self.con = Serial(
             port=serial_port,
             baudrate=19200,
+            bytesize=EIGHTBITS,
+            stopbits=STOPBITS_ONE
         )
         
         self.con.open()
+        
+        self.lock = Lock()
+        
+        lights = []
+        
+        for x in range(LIGHTCOUNT):
+            lights.append(FnordLight(self, x))
+            
+        self.sync()
+        self.stop()
+        
+    
+    def getFnordLight(self, number):
+        
+        return self.lights[number]
     
     
     def setRGB(self, r, g, b):
+        
+        if DEBUG:
+            print("setRGB1 (%s, %s, %s)") % (self.red, self.green, self.blue)
+        
+        self.red = r
+        self.green = g
+        self.blue = b
+        
+        if DEBUG:
+            print("setRGB2 (%s, %s, %s)") % (self.red, self.green, self.blue)
+        
+        
+    def getRGB(self):
+        
+        if DEBUG:
+            print("getRGB (%s, %s, %s)") % (self.red, self.green, self.blue)
+            
+        return (self.red, self.green, self.blue) 
+        
+        
+    def update(self):
+        
+        if DEBUG:
+            print("update (%s, %s, %s)") % (self.red, self.green, self.blue)
+        
+        self.fade_rgb(255, self.red, self.green, self.blue)
+           
+        
+    def flush(self):
+        
+        self.con.flushInput()
+        self.con.flushOutput()
+         
+        
+    def sync(self, addr = 0):
+        
+        self.lock.acquire()
+        
+        for x in range(15):
+            self.con.write( chr(27) )
+            
+        self.con.write( chr(addr) )
+        
+        self.flush()
+        
+        self.lock.release()
+        
+    
+    def zeros(self, count = 8):
+        
+        for x in range(count):
+            self.con.write( chr(0) )
+    
+        
+    def fade_rgb(self, addr, r, g, b, step = 5, delay = 0):
+        
+        self.lock.acquire()
+        
+        self.con.write( chr(addr) )
+        self.con.write(chr(1) )
+        self.con.write( chr(step) )
+        self.con.write( chr(delay) )
+        self.con.write( chr(r) )
+        self.con.write( chr(g) )
+        self.con.write( chr(b) )
+        self.zeros()
+        self.flush()
+        
+        self.lock.release()
+        
+        
+    def stop(self, addr = 255, fading = 1):
+        
+        self.lock.acquire()
+        
+        self.con.write( chr(addr) )
+        self.con.write( chr(8) )
+        self.con.write( chr(fading) )
+        self.zeros(12)
+        self.flush()
+        
+        self.lock.release()
+    
+    
+    def black(self, addr = 255):
+        
+        self.fade_rgb(addr, 0, 0, 0)
+        
+        
+        
+class FnordLight():
+    
+    fnordcontroller = None
+    number = 255
+    
+    red = 0
+    green = 0
+    blue = 0
+    
+    
+    def __init__(self, fnordcontroller, number):
+        
+        self.fnordcontroller = fnordcontroller
+        self.number = number
+
+
+    def black(self):
+        
+        self.fnordcontroller.black(self.number)
+        
+        
+    def setRGB(self, r, g, b):
+        
         self.red = r
         self.green = g
         self.blue = b
         
         
     def getRGB(self):
+        
         return (self.red, self.green, self.blue) 
         
         
     def update(self):
-        self.fade_rgb(255, self.red, self.green, self.blue)
-           
         
-    def flush(self):
-        self.con.flushInput()
-        self.con.flushOutput()
-        
-        
-    def sync(self, addr = 0):
-        
-        for x in range(15):
-            self.con.write("\e")
-            
-        self.con.write( chr(addr) )
-        
-        self.flush()
+        self.fnordcontroller.fade_rgb(self.number, self.red, self.green, self.blue)
         
         
     def fade_rgb(self, addr, r, g, b, step = 5, delay = 0):
-        
-        self.con.write( chr(addr) )
-        self.con.write("\x01")
-        self.con.write( chr(step) )
-        self.con.write( chr(delay) )
-        self.con.write( chr(r) )
-        self.con.write( chr(g) )
-        self.con.write( chr(b) )
-        self.con.write( "\x00\x00\x00\x00\x00" )
-        self.con.write( "\x00\x00\x00" )
-        self.flush()
-        
-        
-    def stop(self, addr = 255, fading = 1):
-        
-        self.con.write( chr(addr) )
-        self.con.write("\x08")
-        self.con.write( chr(fading) )
-        self.con.write( "\x00\x00\x00\x00" )
-        self.con.write( "\x00\x00\x00\x00\x00" )
-        self.con.write( "\x00\x00\x00" )
-        self.flush()
 
+        self.fnordcontroller.fade_rgb(self.number, r, g, b, step, delay)
