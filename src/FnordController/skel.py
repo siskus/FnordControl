fb629ecb7dbffe5e7902c968cea1406eb72f7ef0
Markus Mueller <siskus@gmail.com>
2010-12-20 15:25:16 +0100

    Skeleton for new controllers added

---
+++ src/FnordController/skel.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8-*-
+
+#         Skel
+# A collection of classes to control fnordlights on different levels of 
+# abstraction
+#
+# (c) by Markus Müller <siskus@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+
+import random
+from time import sleep
+
+sys.path.append('..');
+from FnordLib import WorkerBase
+
+class Skel(WorkerBase):
+    
+        
+    def enable(self):
+        
+        #TODO
+        pass
+    
+    
+    def disable(self):
+        
+        #TODO
+        pass
+        
+        
+    def run(self):
+        
+        #TODO
+        pass
+
+
+    def setSpeed(self):
+        
+        #TODO
+        pass
